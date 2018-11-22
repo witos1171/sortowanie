24349510d3da5d290bc5e2427bb596419c2475cb
Jakub <witos1171@gmail.com>
2018-11-22 10:44:03 +0100

    bubleSort

--- sortowanie.py
+++ sortowanie.py
@@ -1,10 +1,14 @@
 
 def BubleSort(list):
-    print("")
-
+    print(list)
+    for x in range(len(list),0,-1):
+        for y in range(x,len(list)-1):
+            if list[y]>list[y+1]:
+                list[y],list[y+1]=list[y+1],list[y]
+    print(list)
 def main():
     dane=input("Podaj dane do sortowania")
     dane=dane.split()
     dane=[int(x) for x in dane]
-    print(dane)
+    BubleSort(dane)
 main()
