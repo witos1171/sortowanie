505bd928b226e42c9ea5ad8f159ab04c3abb9bdb
Jakub <witos1171@gmail.com>
2018-11-22 10:16:47 +0100

    First commit

---
+++ sortowanie.py
@@ -0,0 +1,10 @@
+
+def BubleSort(list):
+    print("")
+
+def main():
+    dane=input("Podaj dane do sortowania")
+    dane=[x.split() for x in dane]
+    dane=[int(x) for x in dane]
+    print(dane)
+main()
